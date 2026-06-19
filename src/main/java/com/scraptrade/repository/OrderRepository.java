f912fae441fc054cfc2bb2da598bd91c0b136f1a
OBEYOP1222 <newlove.doglo@yahoo.com>
2026-06-19 10:44:49 +0000

    Refactor OrderRepository to extend JpaRepository

--- src/main/java/com/scraptrade/repository/OrderRepository.java
+++ src/main/java/com/scraptrade/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.scraptrade.repository;
 
-public class OrderRepository {
+import com.scraptrade.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
